80cf0261284e03dad064c1403dac9e977488d593
satalarais-sys <satalarais@gmail.com>
2026-07-27 15:11:21 -0700

    fix: add Vite config and minimal resources to resolve "Could not resolve entry module 'index.html'" on build

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+// resources/js/app.js
+import './bootstrap';
+
+// Import any JS modules you need here
+console.log('SIM app.js loaded');
+
---
+++ resources/js/bootstrap.js
@@ -0,0 +1,7 @@
+// resources/js/bootstrap.js
+// Minimal bootstrap file for Vite + Laravel
+
+// You can import libraries here (axios, alpinejs, etc.)
+
+// Example: window.axios = require('axios');
+
